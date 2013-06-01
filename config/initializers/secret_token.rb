dd65d4b36cec2051a3a8a8e196c7f9c683c1e0d6
CJ Bryan <christianbryan@gmail.com>
2013-05-31 17:49:28 -0700

    Out with the old, in with the Rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Openurban::Application.config.secret_key_base = '898e743c4c86604507ebae14f89833c0f1756903763f8dff23048aaf7fc61f043eb0145283862ba92429b505c576b29f5e990a6350df5974c3f6486010223cc8'
