182e341b794d42e5013a9d806a088795c4019040
CJ Bryan <christianbryan@gmail.com>
2013-08-09 16:59:58 -0700

    Move JS from view templates to their own controller-specific files.

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -2,12 +2,36 @@ if(typeof(document.controllers) === "undefined") { document.controllers = {}; }
 
 document.controllers["projects"] = {
   common: function () {
-    console.log("projects/common")
+    var map = L.map('map', {
+      center: [51.505, -0.09],
+      zoom: 13
+    });
+
+    L.tileLayer('http://tile.stamen.com/toner/{z}/{x}/{y}.png', {
+      attribution: 'foobar',
+      maxZoom: 20
+    }).addTo(map);
   },
   show: function () {
-    console.log("projects/show")
+
   },
   edit: function () {
-    console.log("projects/edit")
+    // Initialize the FeatureGroup to store editable layers
+    var drawnItems = new L.FeatureGroup();
+    map.addLayer(drawnItems);
+
+    // Initialize the draw control and pass it the FeatureGroup of editable layers
+    var drawControl = new L.Control.Draw({
+        edit: {
+            featureGroup: drawnItems
+        }
+    });
+    map.addControl(drawControl);
+
+    map.on('draw:created', function (e) {
+       last_drawn = e;
+       console.log(e);
+       drawnItems.addLayer(e.layer);
+    });
   }
 }
